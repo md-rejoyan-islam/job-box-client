14d22532ad617451b2499278c4bff446676ff537
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2024-06-25 14:00:08 +0600

    chat crated

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,92 @@
+import { BsArrowReturnRight, BsArrowRightShort } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { useAskQuestionMutation } from "../features/job/jobApi";
+import PropsTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function Chat({ queries }) {
+  const { user } = useSelector((state) => state.userState);
+  const [askQuestion] = useAskQuestionMutation();
+  const { id } = useParams();
+  // ask question
+  const handleAskQuestion = (e) => {
+    e.preventDefault();
+
+    const question = e.target.question.value;
+
+    if (!question) return toast.error("Question is required");
+
+    const data = {
+      email: user?.email,
+      job: id,
+      question,
+    };
+
+    askQuestion(data);
+  };
+  return (
+    <div>
+      <div>
+        <h1 className="text-xl font-semibold text-primary mb-5">General Q&A</h1>
+        <div className="text-primary my-2">
+          {queries?.map(({ question, email, reply, id }) => {
+            return (
+              <div key={id}>
+                <small>{email}</small>
+                <p className="text-lg font-medium">{question}</p>
+                {reply?.map((item, index) => (
+                  <p
+                    className="flex items-center gap-2 relative left-5"
+                    key={index}
+                  >
+                    <BsArrowReturnRight /> {item}
+                  </p>
+                ))}
+
+                {user.role === "employer" && (
+                  <div className="flex gap-3 my-5">
+                    <input
+                      placeholder="Reply"
+                      type="text"
+                      className="w-full "
+                    />
+                    <button
+                      className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
+                      type="button"
+                    >
+                      <BsArrowRightShort size={30} />
+                    </button>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+
+        <form onSubmit={handleAskQuestion}>
+          <div className="flex gap-3 my-5">
+            <input
+              placeholder="Ask a question..."
+              type="text"
+              name="question"
+              className="w-full rounded-md"
+            />
+            <button
+              className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
+              type="submit"
+            >
+              <BsArrowRightShort size={30} />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+Chat.PropsTypes = {
+  queries: PropsTypes.array,
+};
+
+export default Chat;
--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 import meeting from "../assets/meeting.jpg";
-import { BsArrowRightShort, BsArrowReturnRight } from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
 import {
   useApplyForJobMutation,
-  useAskQuestionMutation,
   useGetJobByIdQuery,
 } from "../features/job/jobApi";
 import Loading from "../components/reusable/Loading";
 import { useSelector } from "react-redux";
+import Chat from "./Chat";
 const JobDetails = () => {
   const { id } = useParams();
 
@@ -16,8 +16,6 @@ const JobDetails = () => {
   const [apply] = useApplyForJobMutation();
 
   const { data, isLoading } = useGetJobByIdQuery(id);
-
-  const [askQuestion, { isSuccess }] = useAskQuestionMutation();
 
   const {
     companyName,
@@ -50,135 +48,72 @@ const JobDetails = () => {
     apply(data);
   };
 
-  // ask question
-  const handleAskQuestion = (e) => {
-    e.preventDefault();
-
-    const question = e.target.question.value;
-
-    const data = {
-      email: user?.email,
-      job: id,
-      question,
-    };
-
-    askQuestion(data);
-  };
-
   return (
-    <div className="pt-14 grid grid-cols-12 gap-5">
-      <div className="col-span-9 mb-10">
+    <div className="pt-4 pb-6 grid grid-cols-12 gap-5">
+      <div className="col-span-12 lg:col-span-8 mb-4 lg:mb-10">
         <div className="h-80 rounded-xl overflow-hidden">
           <img className="h-full w-full object-cover" src={meeting} alt="" />
         </div>
         <div className="space-y-5">
           <div className="flex justify-between items-center mt-5">
             <h1 className="text-xl font-semibold text-primary">{position}</h1>
-            <button className="btn" onClick={handleApply}>
+            <button className="btn " onClick={handleApply}>
               Apply
             </button>
           </div>
           <div>
-            <h1 className="text-primary text-lg font-medium mb-3">Overview</h1>
+            <h1 className="text-primary text-lg font-semibold mb-3">
+              Overview
+            </h1>
             <p>{overview}</p>
           </div>
           <div>
-            <h1 className="text-primary text-lg font-medium mb-3">Skills</h1>
+            <h1 className="text-primary text-lg font-semibold mb-3">Skills</h1>
             <ul>
               {skills?.map((skill, index) => (
-                <li className="flex items-center" key={index}>
-                  <BsArrowRightShort /> <span>{skill}</span>
+                <li className="flex items-center gap-1" key={index}>
+                  <BsArrowRightShort />
+                  <span className="pb-[2px]"> {skill}</span>
                 </li>
               ))}
             </ul>
           </div>
           <div>
-            <h1 className="text-primary text-lg font-medium mb-3">
+            <h1 className="text-primary text-lg font-semibold mb-3">
               Requirements
             </h1>
             <ul>
               {requirements?.map((skill, index) => (
-                <li className="flex items-center" key={index}>
-                  <BsArrowRightShort /> <span>{skill}</span>
+                <li className="flex items-center gap-1" key={index}>
+                  <BsArrowRightShort />{" "}
+                  <span className="pb-[2px]">{skill}</span>
                 </li>
               ))}
             </ul>
           </div>
           <div>
-            <h1 className="text-primary text-lg font-medium mb-3">
+            <h1 className="text-primary text-lg font-semibold mb-3">
               Responsibilities
             </h1>
             <ul>
               {responsibilities?.map((skill, index) => (
-                <li className="flex items-center" key={index}>
-                  <BsArrowRightShort /> <span>{skill}</span>
+                <li className="flex items-center gap-1" key={index}>
+                  <BsArrowRightShort />{" "}
+                  <span className="pb-[2px]">{skill}</span>
                 </li>
               ))}
             </ul>
           </div>
         </div>
-        <hr className="my-5" />
-        <div>
-          <div>
-            <h1 className="text-xl font-semibold text-primary mb-5">
-              General Q&A
-            </h1>
-            <div className="text-primary my-2">
-              {queries?.map(({ question, email, reply, id }) => {
-                return (
-                  <div key={id}>
-                    <small>{email}</small>
-                    <p className="text-lg font-medium">{question}</p>
-                    {reply?.map((item, index) => (
-                      <p
-                        className="flex items-center gap-2 relative left-5"
-                        key={index}
-                      >
-                        <BsArrowReturnRight /> {item}
-                      </p>
-                    ))}
+        <hr className="my-5 hidden lg:block" />
 
-                    {user.role === "employer" && (
-                      <div className="flex gap-3 my-5">
-                        <input
-                          placeholder="Reply"
-                          type="text"
-                          className="w-full"
-                        />
-                        <button
-                          className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
-                          type="button"
-                        >
-                          <BsArrowRightShort size={30} />
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-
-            <form onSubmit={handleAskQuestion}>
-              <div className="flex gap-3 my-5">
-                <input
-                  placeholder="Ask a question..."
-                  type="text"
-                  name="question"
-                  className="w-full"
-                />
-                <button
-                  className="shrink-0 h-14 w-14 bg-primary/10 border border-primary hover:bg-primary rounded-full transition-all  grid place-items-center text-primary hover:text-white"
-                  type="submit"
-                >
-                  <BsArrowRightShort size={30} />
-                </button>
-              </div>
-            </form>
-          </div>
+        {/* // Chat component */}
+        <div className="hidden lg:block">
+          <Chat queries={queries} id={id} />
         </div>
       </div>
-      <div className="col-span-3">
-        <div className="rounded-xl bg-primary/10 p-5 text-primary space-y-5">
+      <div className="col-span-12 lg:col-span-4">
+        <div className="rounded-xl border p-4 bg-primary/5 lg:bg-primary/10 lg:p-5 text-primary space-y-5">
           <div>
             <p>Experience</p>
             <h1 className="font-semibold text-lg">{experience}</h1>
@@ -200,7 +135,7 @@ const JobDetails = () => {
             <h1 className="font-semibold text-lg">{location}</h1>
           </div>
         </div>
-        <div className="mt-5 rounded-xl bg-primary/10 p-5 text-primary space-y-5">
+        <div className="mt-5 rounded-xl  border p-4 bg-primary/5 lg:bg-primary/10 lg:p-5 text-primary space-y-5">
           <div>
             <h1 className="font-semibold text-lg">{companyName}</h1>
           </div>
@@ -227,6 +162,10 @@ const JobDetails = () => {
             </a>
           </div>
         </div>
+      </div>
+      {/* // Chat component */}
+      <div className="lg:hidden  col-span-12 ">
+        <Chat queries={queries} id={id} />
       </div>
     </div>
   );
